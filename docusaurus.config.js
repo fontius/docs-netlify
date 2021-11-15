e5ea617c3387be46f425376b51ecdd6349342040
fontius <fontius@gmail.com>
2021-11-15 14:58:03 -0400

    start

---
+++ docusaurus.config.js
@@ -0,0 +1,104 @@
+module.exports = {
+  title: 'Pacio',
+  tagline: 'Future tech that matters',
+  url: 'http://localhost:3000/',  // url where websited is hosted
+  baseUrl: '/',   // path where website is available
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook',  // Usually your GitHub org/user name.
+  projectName: 'docusaurus-netlify',  // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Pacio',
+      logo: {
+        alt: 'Pacio Logo',
+        src: 'img/pacio-logo.png',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/fontius/',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/fontius/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Pacio Core Ltd. All rights reserved.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/fontius/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/fontius/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';   // it is a sudo import, theme will be injected at build time
+import Head from '@docusaurus/Head';
+import Link from '@docusaurus/Link';  // docusaurus exports react-router
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';   // get metadata about page
+import useBaseUrl from '@docusaurus/useBaseUrl';   // gives link after concating website url and path
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Tender',
+    imageUrl: 'img/tender.svg',   // path to image with respect to staic dir
+    description: (
+      <>
+       Zero-Fee Currency enabled by P2P rrDHT via Holochain
+      </>
+    ),
+  },
+  {
+    title: 'Triple Entry Accounting',
+    imageUrl: 'img/tea.svg',
+    description: (
+      <>
+        Reducing fraud and bringing real-time auditing and management to legacy accounting
+      </>
+    ),
+  },
+  {
+    title: 'Pacio Intergrated Business',
+    imageUrl: 'img/pibs.svg',
+    description: (
+      <>
+        Apps of all types incorporating TEA and/or TARI®
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+
+  // Add Netlify Identity script
+  if (typeof window !== "undefined") {
+    // browser code
+    const document = window.document;
+    let script = document.createElement("script");
+    script.innerHTML = `
+      if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/";
+            });
+          }
+        });
+      }
+    `;
+    document.head.appendChild(script);
+  }
+
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
